4ddb476bc2874fbbb22bc8f8137165d79cf08611
ludennis-dlsu <dennis_lu@dlsue.edu.ph>
2020-04-03 18:45:13 +0800

    Completed app

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
+const { isPrivate } = require('../middlewares/checkAuth');
 
 router.get('/', (req, res) => {
+  res.render('home', { pageTitle: 'Home', name: req.session.name } );
+});
+
+
+router.get('/', isPrivate, (req, res) => {
   res.render('home', { pageTitle: 'Home', name: req.session.name } );
 });
 
